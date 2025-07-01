db82f7bef9df8abf215be5366fd427e558c23a7f
GarrgiPhadke <161211502+GarrgiPhadke@users.noreply.github.com>
2025-07-01 08:00:28 -0700

    Delete gallery directory

--- gallery/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import GalleryImage
-
-@admin.register(GalleryImage)
-class GalleryImageAdmin(admin.ModelAdmin):
-    list_display = ('category', 'image', 'uploaded_at')
-    list_filter = ('category',)
